a19e21d75a9499f65948a2edec447475d33b45f0
Zanir Habib <zanir.habib@gmail.com>
2019-09-08 23:35:47 -0700

    looking at json responses

--- test_api.py
+++ test_api.py
@@ -1,7 +1,13 @@
 import requests
+import json
 
 # request = requests.get('http://api.open-notify.org')
 # print(request.text)
 
 people = requests.get('http://api.open-notify.org/astros.json')
-print(people.text)
+# print(people.text)
+
+people_json = people.json()
+# print(people_json)
+
+print("Number of people in space:",people_json['number'])
