47d303d3006d22ce4db95e6c5be061629cde2069
zanirhabib <zanir.habib@gmail.com>
2019-09-18 23:29:38 -0700

    testing whether this commit is signed

--- datamuse/datamuse_api.py
+++ datamuse/datamuse_api.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # parameter = {"rel_rhy":"jingle"}
-# request = requests.get('https://api.datamuse.com/words',parameter)
+# rquest = requests.get('https://api.datamuse.com/words',parameter)
 
 # rhyme_json = request.json()
 # for i in rhyme_json[0:3]:
